5297088e12d6f76448187efaf6c6c0130953dc96
Chayan <20je0291@cse.iitism.ac.in>
2022-05-29 19:22:19 +0530

    third

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,15 +7,18 @@ import Navbar from './Navbar';
 export default function Layout({ children }) {
   return (
     <>
-        <Box maxWidth='1280px' m='auto'>
-            <header>
-                <Navbar />
-            </header>
-            <main>{children}</main>
-            <footer>
-                <Footer />
-            </footer>
-        </Box>
+      <Head>
+        <title>Real Estate</title>
+      </Head>
+      <Box maxWidth='1280px' m='auto'>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </Box>
     </>
   );
 }
